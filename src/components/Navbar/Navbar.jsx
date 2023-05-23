087f479254e48dcf1be166bb794c18e27fededa6
yufus7 <79915956+yufus7@users.noreply.github.com>
2023-05-23 23:02:55 +0300

    fixed navbar and carousel

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 import Logo from '../../assets/logo.png';
@@ -8,30 +8,64 @@ import { GoBell } from 'react-icons/go';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleMouseLeave = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <nav className='navbar'>
+    <nav className='navbar' onMouseLeave={handleMouseLeave}>
       <div className='container nav-container'>
         <div className='nav-left'>
           <img className='nav-logo' src={Logo} alt='' />
           <div className='nav-menu'>
-            <a href='#'>Home</a>
-            <a href='#'>TV Shows</a>
-            <a href='#'>Movies</a>
-            <a href='#'>New & Popular</a>
-            <a href='#'>My List</a>
+            <a href='/#'>Home</a>
+            <a href='/#'>TV Shows</a>
+            <a href='/#'>Movies</a>
+            <a href='/#'>New & Popular</a>
+            <a href='/#'>My List</a>
           </div>
         </div>
         <div className='nav-right'>
-          <GiHamburgerMenu className='nav-icon hamburger' />
+          <GiHamburgerMenu
+            className='nav-icon hamburger'
+            onClick={toggleMobileMenu}
+          />
           <img className='nav-profile' src={Avatar} alt='' />
           <div className='nav-items'>
             <HiOutlineSearch className='nav-icon' />
-            <a href='#'>Kids</a>
-            <a href='#'>DVD</a>
+            <a href='/#'>Kids</a>
+            <a href='/#'>DVD</a>
             <GoBell className='nav-icon' />
           </div>
         </div>
       </div>
+      {isMobileMenuOpen && (
+        <div className='mobile-menu'>
+          <ul>
+            <li>
+              <a href='/#'>Home</a>
+            </li>
+            <li>
+              <a href='/#'>TV Shows</a>
+            </li>
+            <li>
+              <a href='/#'>Movies</a>
+            </li>
+            <li>
+              <a href='/#'>New & Popular</a>
+            </li>
+            <li>
+              <a href='/#'>My List</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
