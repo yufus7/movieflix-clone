fe48bfc89a4d2858c5bbd2fb4981ee32a3d98855
yufus7 <79915956+yufus7@users.noreply.github.com>
2023-05-23 21:53:14 +0300

    updated css

--- src/components/Trend/Trend.jsx
+++ src/components/Trend/Trend.jsx
@@ -9,155 +9,69 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+import movieData from '../../assets/movie/movieData.json';
+
 function Trend() {
   return (
     <div className='trend'>
       <div className='container trend-container'>
         <h2>Trending Now</h2>
-        <article>
+        <div>
           <Swiper
             autoplay={false}
             slidesPerView={6}
+            slidesPerGroup={1}
             spaceBetween={10}
             pagination={{
               clickable: true,
             }}
             modules={[Pagination]}
             className='mySwiper'
-            // breakpoints={{
-            //   576: {
-            //     width: 576,
-            //     slidesPerView: 1,
-            //   },
-            //   768: {
-            //     slidesPerView: 2,
-            //   },
-            //   992: {
-            //     width: 992,
-            //     slidesPerView: 3,
-            //   },
-            //   1200: {
-            //     width: 1200,
-            //     slidesPerView: 4,
-            //   },
-            //   1400: {
-            //     width: 1400,
-            //     slidesPerView: 5,
-            //   },
-            // }}
+            breakpoints={{
+              0: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                spaceBetween: 10,
+              },
+              321: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+                spaceBetween: 10,
+              },
+              641: {
+                slidesPerView: 2,
+                slidesPerGroup: 1,
+                spaceBetween: 15,
+              },
+              961: {
+                slidesPerView: 3,
+                slidesPerGroup: 1,
+                spaceBetween: 15,
+              },
+              1281: {
+                slidesPerView: 4,
+                slidesPerGroup: 1,
+                spaceBetween: 20,
+              },
+              1601: {
+                slidesPerView: 5,
+                slidesPerGroup: 1,
+                spaceBetween: 20,
+              },
+            }}
           >
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://c4.wallpaperflare.com/wallpaper/294/512/920/tv-show-stranger-things-caleb-mclaughlin-charlie-heaton-wallpaper-preview.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://leadergamer.com.tr/app/uploads/2019/12/the_witcher_netflix_promos-2.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://22dakika.org/wp-content/uploads/2021/07/Ask-101-afis.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://i0.wp.com/i0.wp.com/dizigom.tv/wp-content/uploads/2023/01/women-at-war-bolum-63352329.jpg?resize=231,130?resize=500,282'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://cdn1.img.sputniknews.com.tr/img/103717/72/1037177295_0:0:1280:720_1920x0_80_0_0_689cb2eafc14e74de43cdd0345cd34fe.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://c4.wallpaperflare.com/wallpaper/294/512/920/tv-show-stranger-things-caleb-mclaughlin-charlie-heaton-wallpaper-preview.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://leadergamer.com.tr/app/uploads/2019/12/the_witcher_netflix_promos-2.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://22dakika.org/wp-content/uploads/2021/07/Ask-101-afis.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://i0.wp.com/i0.wp.com/dizigom.tv/wp-content/uploads/2023/01/women-at-war-bolum-63352329.jpg?resize=231,130?resize=500,282'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
-            <SwiperSlide className='carousel-slide'>
-              <img
-                src='https://cdn1.img.sputniknews.com.tr/img/103717/72/1037177295_0:0:1280:720_1920x0_80_0_0_689cb2eafc14e74de43cdd0345cd34fe.jpg'
-                alt='1'
-              />
-              <article className='carousel-movie-details'>
-                <h3>Movie Name</h3>
-                <h4>Quality: HD</h4>
-                <button>Watch Now</button>
-              </article>
-            </SwiperSlide>
+            {movieData.map((movie, index) => (
+              <SwiperSlide className='carousel-slide' key={index}>
+                <img src={movie.image} alt={movie.title} />
+                <div className='carousel-movie-details'>
+                  <h3>{movie.title}</h3>
+                  <h4>Quality: {movie.quality}</h4>
+                  <button>Watch Now</button>
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
-        </article>
+        </div>
       </div>
     </div>
   );
